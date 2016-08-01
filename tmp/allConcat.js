8d801140a863e8d2c5794861aec7303ccbf77058
Ben Logue and Carl Egbert <ben@>
2016-08-01 10:09:32 -0700

    refactored calculator interface.

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -12,25 +12,62 @@ $(document).ready(function(event) {
     });
   });
 
-  $("#add").click(function(event) {
+  $(".calculator-button").click(function(event) {
     event.preventDefault();
     $("#solution").empty();
     var num1 = parseInt($("#num1").val());
     var num2 = parseInt($("#num2").val());
+    var calculationType = $(this).attr("value");
     var simpleCalculator = new Calculator();
-    var output = simpleCalculator.add(num1, num2);
+    var output;
+    switch(calculationType) {
+      case "add":
+        output = simpleCalculator.add(num1, num2);
+        break;
+      case "subtract":
+        output = simpleCalculator.subtract(num1, num2);
+        break;
+      case "multiply":
+        output = simpleCalculator.multiply(num1, num2);
+        break;
+      case "divide":
+        output = simpleCalculator.divide(num1, num2);
+        break;
+      default:
+        output = 0;
+    }
     $('#solution').append("<li>" + output + "</li>");
   });
 
-  $("#sub").click(function(event){
-    event.preventDefault();
-    $("#solution").empty();
-    var num1 = parseInt($("#num1").val());
-    var num2 = parseInt($("#num2").val());
-    var simpleCalculator = new Calculator();
-    var output = simpleCalculator.subtract(num1, num2);
-    $('#solution').append("<li>" + output + "</li>");
-  });
+  // $("#sub").click(function(event){
+  //   event.preventDefault();
+  //   $("#solution").empty();
+  //   var num1 = parseInt($("#num1").val());
+  //   var num2 = parseInt($("#num2").val());
+  //   var simpleCalculator = new Calculator();
+  //   var output = simpleCalculator.subtract(num1, num2);
+  //   $('#solution').append("<li>" + output + "</li>");
+  // });
+  //
+  // $("#mult").click(function(event){
+  //   event.preventDefault();
+  //   $("#solution").empty();
+  //   var num1 = parseInt($("#num1").val());
+  //   var num2 = parseInt($("#num2").val());
+  //   var simpleCalculator = new Calculator();
+  //   var output = simpleCalculator.multiply(num1, num2);
+  //   $('#solution').append("<li>" + output + "</li>");
+  // });
+  //
+  // $("#divide").click(function(event){
+  //   event.preventDefault();
+  //   $("#solution").empty();
+  //   var num1 = parseInt($("#num1").val());
+  //   var num2 = parseInt($("#num2").val());
+  //   var simpleCalculator = new Calculator();
+  //   var output = simpleCalculator.divide(num1, num2);
+  //   $('#solution').append("<li>" + output + "</li>");
+  // });
 });
 
 
